d9454a219bb88196cf4fc888eaab9ef80c4f5b90
zzhanna <foto1drug@gmail.com>
2021-08-10 18:00:13 +0200

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const edinicy = ['', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine'];
+    const desytki = ['', ' ten', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
+    const dopolnit = ['', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+    let result = '';
+    let ed, des, sot;
+    if (number === 0) {
+        return `zero`;
+    }
+    sot = Math.trunc(number / 100);
+    if (sot === 0) result = '';
+    if (sot > 0) {
+        result = result + (edinicy[sot] + ' hundred');
+    }
+    ed = number % 10;
+    des = ((number % 100) - ed) / 10;
+    if (des === 1 && (ed > 0 && ed <= 9)) {
+        result = result + dopolnit[ed];
+    } else {
+        result = result + desytki[des] + edinicy[ed];
+    }
+    return result.trim();
 }
